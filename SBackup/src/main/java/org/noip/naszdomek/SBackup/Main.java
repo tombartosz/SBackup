ecf052f853ca32c772d39dc18d6a80bb95495d41
tombartosz <tombartosz@gmail.com>
2014-02-04 14:27:17 +0100

    Little upgrade

--- SBackup/src/main/java/org/noip/naszdomek/SBackup/Main.java
+++ SBackup/src/main/java/org/noip/naszdomek/SBackup/Main.java
@@ -1,30 +1,28 @@
 package org.noip.naszdomek.SBackup;
 
 import org.noip.naszdomek.SBackup.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
-/**
- * Hello world!
- *
- */
+@Component
 public class Main 
 {
+	@Autowired
 	private Config config;
+	
+	@Autowired
 	private App app;
 	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
-        
+              
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
                
-        Main main = new Main();
+        Main main = context.getBean(Main.class);
         
-        main.config = context.getBean(Config.class);
         main.config.setCmdParams(args);
-        
-        main.app = context.getBean(App.class);
         main.app.run();
         
         context.close();
